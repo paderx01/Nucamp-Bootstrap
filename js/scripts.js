dde8169cc56a684e3300bf94286b431ffd4173de
Pader Xiong <paderx26@gmail.com>
2022-11-12 12:24:28 -0600

    week 4 assignments

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,13 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     }); 
+    $('#loginButton').click(function (){
+        $("#loginModal").modal('show');
+    });
+
+    $('#reserveButton').click(function (){
+        $("#reserveModal").modal('show');
+    });
+
 }); 
+
